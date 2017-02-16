de78d85d09bc25a209a8029ffeb81e6bf0427bd9
Arko Gupta <arkogupta16@gmail.com>
2017-02-17 00:31:46 +0530

    Update LyricsFetcher.py
    Added exception handling for http errors

--- LyricsFetcher.py
+++ LyricsFetcher.py
@@ -43,11 +43,24 @@ def RemoveSpaces(A):
 def main():
 
 	
-	artist = raw_input('Enter name of artist : ')
-	artist = RemoveSpaces(artist.lower())
-	song_name = raw_input('Enter name of song : ')
-	song_name = RemoveSpaces(song_name.lower())
-	url = 'http://www.azlyrics.com/lyrics/'+artist + '/'+ song_name+'.html'
+
+	
+	try:
+		artist = raw_input('Enter name of artist : ')
+		artist = RemoveSpaces(artist.lower())
+		song_name = raw_input('Enter name of song : ')
+		song_name = RemoveSpaces(song_name.lower())
+		url = 'http://www.azlyrics.com/lyrics/'+artist + '/'+ song_name+'.html
+		urllib2.urlopen(url)
+	except urllib2.HTTPError , err:
+		if err.code == 404:
+			print 'Please check the name of the artist and the song.'
+			print 'The url',url,'does not exist'
+		elif err.code == 403:
+			print 'Access Denied!'
+		else:
+			print 'The following error occured : err',err.code
+		
 	print 'Fetching lyrics for song "' + song_name + ' "by '+ artist + '...' 
 	GetLyrics(url,song_name+'.txt',artist)
 	print 'Lyrics saved in file "'+ song_name+'.txt" '
